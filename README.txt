0ba0a07468e38cdbf1eaeebef1bf2906834f0032
mikexie360 <mxie360@gmail.com>
2021-09-22 20:10:56 -0500

    2
    1

--- README.txt
+++ README.txt
@@ -1,5 +1,6 @@
 Mike Xie
 mxx170002@utdallas.edu
+AI CS6364.001
 HW1
 This is my submission for Homework 1
 
@@ -18,8 +19,13 @@ cd into the aima code and  install numpy in your terminal
 
 ------------------------------------------------
 p1.py is for kitty world with ability to control kitty
+    does not solve any particular problem but is for problem 1
 p2.py is for missionary and cannibal problem with various search aglorithms
+    solves Problem 2.1 and 2.2
 p3.py is for roadmap problem with RBFS and Astar
+    Solves Problem 3.1 , 3.2 , 3.3 and 3.4
+p4.py is for extra credit in problem 3
+    Solves Problem 3 extra-credit
 
 --------------------------------------------------
 All python files are using python 3.
@@ -35,8 +41,13 @@ to run p3.py run
     python3 p3.py
 you will then see the solution path for roadproblem with RBFS and Astar
 
+to run p4.py run
+    python3 p4.py
+You will then see that the hueristic is inconsistent
 ------------------------------------------------------
 All python runs on AIMA code, but some modifcations may have been made to into
 p1.py kitty program may not have been needed.
 
 For p3.py , I am assuming the direct flight distance is a good heuristic.
+
+p4.py shows that flight distance is actually a pretty bad heuristic for road path.
---
+++ p4.py
@@ -0,0 +1,127 @@
+#Mike Xie
+# this program tests the heuristic of the flight distance and road map
+from numpy import array
+from utils import *
+from search import *
+
+#flight path to dallas
+"""
+Austin          182
+Charlotte       929
+San Francisco   1230
+Los Angeles     1100
+New York        1368
+Chicago         800
+Seattle         1670
+Santa Fe        560
+Bakersville     1282
+Boston          1551
+"""
+#flight path to dallas in order of distance
+"""
+"""
+flightPath=[
+        ["Dallas", 0],
+        ["Austin",182],
+        ["Charlotte",929],
+        ["San Francisco", 1230],
+        ["Los Angeles", 1100],
+        ["New York",1368],
+        ["Chicago", 800],
+        ["Seattle",1670],
+        ["Santa Fe",560],
+        ["Bakersville",1282],
+        ["Boston", 1551]]
+
+def bubbleSort(flight):
+    length = len(flight)
+
+    for i in range(length -1):
+        for j in range(0, length - i -1):
+            if flight[j][1] > flight[(j+1)][1]:
+                flight[j], flight[(j+1)] = flight[(j+1)], flight[j]
+
+class Graph():
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+    def printSolution(self, dist):
+        print("Dallas to other cities through road")
+        for node in range(self.V):
+            print("Dallas to",flightPath[node][0],"road distance",flightPath[node][1], "flight distance", dist[node])
+    def minDistance(self, dist, sptSet):
+ 
+        # Initialize minimum distance for next node
+        min = sys.maxsize
+ 
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+ 
+        return min_index
+
+    def dijkstra(self, src):
+ 
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+ 
+        for cout in range(self.V):
+            u = self.minDistance(dist, sptSet)
+            sptSet[u] = True
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]:
+                    dist[v] = dist[u] + self.graph[u][v]
+ 
+        self.printSolution(dist)
+
+def main():
+    
+
+    # now sort it to get in order
+    bubbleSort(flightPath)
+    # sorted flight path looks like this
+    """
+    [['Dallas', 0],
+    ['Austin', 182], 
+    ['Santa Fe', 560], 
+    ['Chicago', 800], 
+    ['Charlotte', 929], 
+    ['Los Angeles', 1100], 
+    ['San Francisco', 1230], 
+    ['Bakersville', 1282], 
+    ['New York', 1368], 
+    ['Boston', 1551], 
+    ['Seattle', 1670]]
+    """
+    # now flight path is in order
+    print(flightPath, "\n")
+    
+    roadPath=[
+        # Dallas, Austin, Santa Fe, Chicago, Charlotte, Los Angeles, San Francisco, Bakersville, New York, Boston, Seattle
+        [0,         195,    640,    0,          0,      0,              0,              0,          1548,   0,       0], # Dallas
+        [195,       0,       0,      0,         1200,   1377,           0,          0,              0,      1963,   0], # Austin
+        [640,       0,       0,      1272,      0,      0,              0,              864,          0,         0,   1463], # Santa Fe
+        [0,         0,      1272,          0,      0,      0,              0,              0,        0,       983,   2064], # Chicago
+        [0,         1200,      0,          0,      0,          0,          0,              0,         634,        0,       0,], # Charlotte
+        [0,         1377,          0,       0,        0,       0,          383,          153,          0,          0,      0], # Los Angeles
+        [0,         0,          0,      0,          0,      383,          0,              283,          0,      3095,      807], # San Francisco
+        [0,         0,          864,      0,          0,       153,             283,       0,          0,       0,         0],  # Bakersville
+        [1548,         0,          0,          0,     634,         0,          0,              0,       0,      225,      0], # New York
+        [0,         1963,       0,       983,      0,      0,          3095,              0,        225,    0,           0], # Boston
+        [0,         0,          1463,      2064,      0,          0,          807,          0,              0,         0,       0]  # Seattle
+    ]
+    r = Graph(11)
+    r.graph = roadPath
+
+    r.dijkstra(0)   # all path costs to dallas
+
+    print("""
+    The heuristic is inconsistent.
+    You can see that although Charlotte has higher hueristic/flight distance than Chicago, 
+    Charlotte's road path to dallas is actually shorter than Chicago's road path to dallas.
+    Making the heuristic inconsistent
+    """)
+main()
